27b35d1404870db5cce74809eaafe92e7ca3caa7
rohit-541 <rohitkumar240iit@gmail.com>
2025-02-08 16:14:59 +0530

    Added correct backend Address

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    baseURL: 'http://localhost:3000',
-    baseURL2: 'http://127.0.0.1:3000',
+    baseURL: 'http://10.17.7.154:3000',
+    baseURL2: 'http://10.17.7.154:3000'
   };
   
   export default config;
